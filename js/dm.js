c489ca2c64d3a2e4434ca3ff6539a494f640c932
Bruce Brown <bbrown32@student.cvtc.edu>
2022-02-23 16:28:04 -0600

    Updated dm.js to have a switch case for setting the flags.

--- js/dm.js
+++ js/dm.js
@@ -7,7 +7,20 @@ display the data in result fields, and any other DOM manipulation.
 // Event listener for the submit button contains the logic for DOM manipulation.
 document.querySelector("#convertForm").addEventListener("submit", function() {
   
-  // TODO get and set data for DOM manipulation
-  
+  // Sets the flag for the current or country selected
+  switch(document.querySelector("#UserConvertCurrency").value) {
+    
+    case Pesos:
+    // TODO Add logic for flag
+    break;
+    
+    case Euros:
+    // TODO Add logic for flag
+    break;
+    
+    case RMB:
+    // TODO Add logic for flag
+    break;
+  }
   
 });
