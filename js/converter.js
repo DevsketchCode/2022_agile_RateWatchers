362dd1be37b7a5db188fc2e2dc84c1c6ce8f66bf
Bruce Brown <bbrown32@student.cvtc.edu>
2022-02-23 15:46:42 -0600

    [Delivers #181320234] Added JavaScript logic to grab the data from the input field when submitted and validate only numbers are entered. Code will refresh the input field if invalid data is inputted.

--- js/converter.js
+++ js/converter.js
@@ -1,26 +1,22 @@
-// Will wait until the document is ready before starting up the jQuery.
-// Since document is jQuery's default selector, we can omit it and
-// if document is selected, then ready() is the default method so we
-// can omit it as well.
-$(function() {
-  // Click event listener function for submit button
-  $('#UserSubmit').click(function(e) {
-    
-    // Prevents the default action of refreshing the page
-    e.preventDefault();
-    
-    //Sets the user input to a variable if a number
-    // otherwise will alert the user that they are incorrect
-    if(Number($('#UserInputAmount'))) {
-      $inputCurrency = $('#UserInputAmount');
-      
-      
-    }
-    else {
-      alert('Please only enter numbers!');
-    }
-    
-  });
+/*
+**********
+Script is designed to get the data from the submission form,
+validate the data, convert the currencies, display flags,
+and display the data in result fields.
+*/
+
+// Event listener for the submit button contains the logic for converting amounts
+// as well as DOM manipulation.
+document.querySelector("#convertForm").addEventListener("submit", function() {
   
+  // Gets the text from the input field 
+  var userInput = Number(document.querySelector("#UserInputAmount").value);
   
+  // Validates only numbers are submitted and refreshes the input field
+  // when invalid data is submitted.
+  if(userInput == '' || isNaN(userInput) ){
+    alert("Make sure you only enter a number!");
+    document.querySelector("#UserInputAmount").value = "";
+  }
+
 });
