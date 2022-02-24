420c2d589b9de42cc53f9035192c8001525f708a
Bruce Brown <bbrown32@student.cvtc.edu>
2022-02-24 11:46:25 -0600

    [Delivers #181320188] Added IDs to the result field labels. Added logic to dm.js to display the flags of the origin and new flags in the results.

--- js/dm.js
+++ js/dm.js
@@ -7,20 +7,46 @@ display the data in result fields, and any other DOM manipulation.
 // Event listener for the submit button contains the logic for DOM manipulation.
 document.querySelector("#convertForm").addEventListener("submit", function() {
   
+  var countrySelected = document.querySelector("#UserConvertCurrency").value
+  var originCountryFlag = "usa";
+  var newCountryFlag;
+  
+  // Test for country currency selected
+  //alert("Country is " + countrySelected);
+  
   // Sets the flag for the current or country selected
-  switch(document.querySelector("#UserConvertCurrency").value) {
+  switch(countrySelected) {
     
-    case Mexico:
-    // TODO Add logic for flag
+    case "Mexico":
+    newCountryFlag = "mexico"
     break;
     
-    case Canada:
-    // TODO Add logic for flag
+    case "Canada":
+    newCountryFlag = "canada"
     break;
     
-    case China:
-    // TODO Add logic for flag
+    case "China":
+    newCountryFlag = "china"
     break;
   }
   
+  // Test for country flag selected
+  //alert("Country is " + newCountryFlag);
+  
+  // Sets the flag labels in the results to blank
+  var originFlagLabel = document.querySelector("#OriginFlag");
+  var newFlagLabel = document.querySelector("#NewFlag");
+  
+  originFlagLabel.innerHTML = "";
+  newFlagLabel.innerHTML = "";
+  
+  // Displays the flags of the origin currency and new currency
+  var originFlagImage = document.createElement("img");
+  var newFlagImage = document.createElement("img");
+  
+  originFlagImage.setAttribute("src", "images/" + originCountryFlag + "Flag.png");
+  newFlagImage.setAttribute("src", "images/" + newCountryFlag + "Flag.png");
+  
+  originFlagLabel.appendChild(originFlagImage);
+  newFlagLabel.appendChild(newFlagImage);
 });
