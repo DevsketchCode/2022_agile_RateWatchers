ecefe2d63ea52d7b4f7d160c10e933e9675e1d0b
David Oberlander <david.oberlander@student.cvtc.edu>
2022-02-25 00:50:45 -0600

    [delivers #181320194] Hid Fields at Start, Progressively display, User Data applied to Results field.

--- js/dm.js
+++ js/dm.js
@@ -4,17 +4,45 @@ Script is designed to get the data from the submission form, display flags,
 display the data in result fields, and any other DOM manipulation.
 */
 
+/* -- DOM FUNCTIONS -- */
+// Show field function
+function ShowField(id) {
+  id.style.display = "block";
+}
+
+// Fill Results Fields
+function FillConversionResultsTable(amountEntered, originCurrency, newCurrency) {
+  document.querySelector("#AmountEntered").innerHTML = amountEntered;
+  document.querySelector("#OriginCurrencyName").innerHTML = originCurrency;
+  document.querySelector("#NewCurrencyName").innerHTML = newCurrency;
+}
+
+// Display Country Conversion if data entered in currency field
+document.querySelector("#UserInputAmount").addEventListener("keypress", (event) => {
+  currencyCountryDiv = document.querySelector("#CurrencyCountryDiv");
+  if(currencyCountryDiv.style.display === "none" || 
+      currencyCountryDiv.style.display === "") {
+    // Display the currency Country div
+    ShowField(currencyCountryDiv);
+  }
+});
+
+
 // Event listener for the submit button contains the logic for DOM manipulation.
 document.querySelector("#convertForm").addEventListener("submit", function() {
-  
+
+  // Get Values Form User Input
+  var amountEntered = document.querySelector("#UserInputAmount").value;
+  var originCurrencyName = document.querySelector("#OriginalCurrencyName").innerHTML;
   var countrySelected = document.querySelector("#UserConvertCurrency").value;
+  
+  // Get Selected Convert To Currency Text
+  var selectedToCurrency = document.querySelector("#UserConvertCurrency");
+  var newCurrencyName = selectedToCurrency.options[selectedToCurrency.selectedIndex].text;
+
   var originCountryFlag = "usa";
   var newCountryFlag;
-  
-  // Test for country currency selected
-  //alert("Country is " + countrySelected);
-  
-  // Sets the flag for the current or country selected
+    // Sets the flag for the current or country selected
   switch(countrySelected) {
     
     case "Mexico":
@@ -29,10 +57,7 @@ document.querySelector("#convertForm").addEventListener("submit", function() {
     newCountryFlag = "china";
     break;
   }
-  
-  // Test for country flag selected
-  //alert("Country is " + newCountryFlag);
-  
+
   // Sets the flag labels in the results to blank
   var originFlagLabel = document.querySelector("#OriginFlag");
   var newFlagLabel = document.querySelector("#NewFlag");
@@ -50,4 +75,11 @@ document.querySelector("#convertForm").addEventListener("submit", function() {
   originFlagLabel.appendChild(originFlagImage);
   newFlagLabel.appendChild(newFlagImage);
 
+  // Populate the Results Fields
+  FillConversionResultsTable(amountEntered, originCurrencyName, newCurrencyName);
+
+  // Display Converted Results Div on Convert Button Click
+  resultsDiv = document.querySelector("#ConvertedResults");
+  ShowField(resultsDiv);
 });
+
