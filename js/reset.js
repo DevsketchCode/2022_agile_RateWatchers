5679019914e2b04a12c5f0b84a75b416645c0a13
David Ding <dding@student.cvtc.edu>
2022-03-05 16:42:42 -0600

    [delivers #181320203] Added a clear form option to the webpage.

---
+++ js/reset.js
@@ -0,0 +1,23 @@
+//Ask user to confirm if they want to reset the form.
+function startResetForm() {
+    //Ask user if they want to reset the fields.
+    if (confirm("All values will be reset to default. You will lose your entries that you input. Are you sure you want to reset the form?") == true) {
+        hideConvertTo();
+        resetForm();
+    } else {
+        alert("You pressed Cancel.");
+    }
+}
+
+//Reset the entire form.
+function resetForm() {
+    document.getElementById("convertForm").reset();
+    alert("Form is reset");
+}
+
+//Hide fields.
+function hideConvertTo() {
+    //Get the div for the field where user selects a country, then hide in next line.
+    var convertSettings = document.getElementById("input-convert-to-country-div");
+    convertSettings.style.display = "none";
+}
